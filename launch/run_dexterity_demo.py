0bfc791b7f1599dfd2abd9dadf9ab8dd451e2021
Mumbzzz <mwhidby@g.ucla.edu>
2024-10-01 15:10:26 -0700

    Evans bugs fixed

--- launch/run_dexterity_demo.py
+++ launch/run_dexterity_demo.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -10,14 +11,28 @@ def generate_launch_description():
         'default_params.yaml'
     )
 
-    print(config)
+    # Print the path to the YAML file
+    print(f"Loading configuration from: {config}")
+    
+    # Read and parse the YAML file
+    with open(config, 'r') as file:
+        config_params = yaml.safe_load(file)
+
+    # Print the content of the YAML file
+    print(config_params)
 
     return LaunchDescription([
         Node(
             package='leap_ros2',
+            executable='leaphand_node.py',
+            name='leaphand_node',
+            parameters=[config_params]  # Pass the parsed parameters here
+        ),
+        Node(
+            package='leap_ros2',
             executable='demo_repeat_joint_data.py',
             name='demo_repeat_joint_data',
-            parameters=[config]
+            parameters=[config_params]
         ),
     ])
 
